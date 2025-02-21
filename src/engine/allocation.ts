337dde7c7f1c9bc8b8bdbb3abb08c55d42b6a452
Kasekopf <jeffreydudek@gmail.com>
2025-02-20 21:21:57 -0800

    Refactor allocators

--- src/engine/allocation.ts
+++ src/engine/allocation.ts
@@ -3,10 +3,91 @@ import { inHardcore, myTurncount, pullsRemaining, runCombat } from "kolmafia";
 import { args } from "../args";
 import { stableSort } from "../lib";
 import { summonSources } from "../resources/summon";
-import { Allocations, DeltaTask, Task } from "./task";
-import { get, sum, undelay } from "libram";
+import { Allocation, Allocations, AllocationSummon, DeltaTask, Task } from "./task";
+import { get, undelay } from "libram";
 import { forceNCSources, noncombatForceNCSources } from "../resources/forcenc";
 import { Priorities } from "./priority";
+
+type Allocator = {
+  applies: (which: Allocation) => boolean;
+  amount: () => number;
+  delta: DeltaTask | ((which: Allocation) => DeltaTask);
+};
+
+const allocators: Allocator[] = [
+  // Pulls
+  {
+    applies: (which) => which === Allocations.Pull,
+    amount: () => {
+      if (inHardcore() || myTurncount() >= 1000) return 0;
+      return pullsRemaining() - (20 - args.major.pulls);
+    },
+    delta: {
+      tag: "Pull",
+    },
+  },
+  // NC Forcers
+  {
+    applies: (which) => which === Allocations.NCForce,
+    amount: () => (get("noncombatForcerActive") ? 1 : 0),
+    delta: {
+      tag: "NCForce",
+      replace: { priority: () => Priorities.GoodForceNC },
+    },
+  },
+  ...noncombatForceNCSources.map(
+    (s) =>
+      <Allocator>{
+        applies: (which) => which === Allocations.NCForce,
+        amount: () => s.remaining(),
+        delta: {
+          tag: "NCForce",
+          amend: {
+            prepare: (original) => () => {
+              original?.();
+              s.do();
+            },
+          },
+        },
+      }
+  ),
+  ...forceNCSources.map(
+    (s) =>
+      <Allocator>{
+        applies: (which) => which === Allocations.NCForce,
+        amount: () => s.remaining(),
+        delta: {
+          tag: "NCForce",
+          replace: { priority: () => Priorities.BadForcingNC },
+        },
+      }
+  ),
+  // Summons
+  ...summonSources.map(
+    (s) =>
+      <Allocator>{
+        applies: (which: Allocation) => typeof which === "object" && "summon" in which,
+        amount: () => s.available(),
+        delta: (req) =>
+          <DeltaTask>{
+            replace: {
+              do: () => {
+                // Perform the actual summon
+                debug(`Summon source: ${s.name}`);
+                s.summon((req as AllocationSummon).summon);
+                runCombat();
+              },
+            },
+            amend: {
+              ready: (originalReady) => {
+                return () => (originalReady?.() ?? true) && (s.ready?.() ?? true);
+              },
+            },
+            tag: s.name,
+          },
+      }
+  ),
+];
 
 export function allocateResources(tasks: Task[]): Map<string, DeltaTask> {
   const resourcesAllocated = new Map<string, DeltaTask>();
@@ -15,89 +96,20 @@ export function allocateResources(tasks: Task[]): Map<string, DeltaTask> {
     resourcesNeeded,
     (task) => -1 * (undelay(task.resources)?.value ?? 0)
   );
-  let pullsLeft = pullsRemaining() - (20 - args.major.pulls);
-  if (inHardcore() || myTurncount() >= 1000) pullsLeft = 0; // No pulls in hardcore or out of ronin
 
-  const summonsLeft = summonSources.map((s) => s.available());
-
-  let activeForceNCLeft = get("noncombatForcerActive") ? 1 : 0;
-  let preparableForceNCLeft = sum(forceNCSources, (s) => s.remaining());
-  const passiveForceNCLeft = noncombatForceNCSources.map((s) => s.remaining());
-
+  const remaining = allocators.map((s) => s.amount());
   for (const task of tasksByResource) {
     const resources = undelay(task.resources);
     if (!resources) break;
-    let allocated = false;
-    if (resources.which === Allocations.Pull) {
-      if (pullsLeft > 0) {
-        resourcesAllocated.set(task.name, {
-          tag: "Pull",
-        });
-        pullsLeft -= 1;
-        allocated = true;
-      }
-    } else if (resources.which === Allocations.NCForce) {
-      if (activeForceNCLeft > 0) {
-        resourcesAllocated.set(task.name, {
-          tag: "NCForce",
-          replace: { priority: () => Priorities.GoodForceNC },
-        });
-        activeForceNCLeft -= 1;
-        allocated = true;
-      }
-      if (!allocated) {
-        for (let i = 0; i < passiveForceNCLeft.length; i++) {
-          if (!passiveForceNCLeft[i]) continue;
-          const passiveForceNC = noncombatForceNCSources[i];
-          resourcesAllocated.set(task.name, {
-            tag: "NCForce",
-            amend: {
-              prepare: (original) => () => {
-                original?.();
-                passiveForceNC.do();
-              },
-            },
-          });
-          allocated = true;
-        }
-      }
-      if (!allocated && preparableForceNCLeft > 0) {
-        resourcesAllocated.set(task.name, {
-          tag: "NCForce",
-          replace: { priority: () => Priorities.BadForcingNC },
-        });
-        preparableForceNCLeft -= 1;
-        allocated = true;
-      }
-    } else if ("summon" in resources.which) {
-      for (let i = 0; i < summonsLeft.length; i++) {
-        if (!summonsLeft[i]) continue;
-        if (summonSources[i].canFight(resources.which.summon)) {
-          const allocatedSummon = summonSources[i];
-          const monster = resources.which.summon;
-          resourcesAllocated.set(task.name, {
-            replace: {
-              do: () => {
-                // Perform the actual summon
-                debug(`Summon source: ${allocatedSummon.name}`);
-                allocatedSummon.summon(monster);
-                runCombat();
-              },
-            },
-            amend: {
-              ready: (orignalReady) => {
-                return () => (orignalReady?.() ?? true) && (allocatedSummon.ready?.() ?? true);
-              },
-            },
-            tag: allocatedSummon.name,
-          });
-          allocated = true;
-          break;
-        }
-      }
-    }
-
-    if (!allocated && resources.required) {
+    const found = allocators.findIndex((a, i) => a.applies(resources.which) && remaining[i] > 0);
+    if (found >= 0) {
+      const delta =
+        typeof allocators[found].delta === "function"
+          ? allocators[found].delta(resources.which)
+          : allocators[found].delta;
+      remaining[found] -= 1;
+      resourcesAllocated.set(task.name, delta);
+    } else {
       resourcesAllocated.set(task.name, UNFULFILLED_ALLOCATION);
     }
   }
