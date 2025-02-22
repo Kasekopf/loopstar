8a8c804f4ae27e12a81de91ff6310fc4c568c033
Kasekopf <jeffreydudek@gmail.com>
2025-02-22 00:23:00 -0800

    Once we know the ore we just check that we have it

--- src/tasks/level8.ts
+++ src/tasks/level8.ts
@@ -240,11 +240,16 @@ export function oresNeeded(): number {
   if (step("questL08Trapper") >= 2) return 0;
   if (trainSetAvailable()) return 0;
   let ore_needed = 3;
-  ore_needed -= Math.min(
-    itemAmount($item`asbestos ore`),
-    itemAmount($item`chrome ore`),
-    itemAmount($item`linoleum ore`)
-  );
+  const ore = get("trapperOre");
+  if (ore) {
+    ore_needed -= itemAmount(ore);
+  } else {
+    ore_needed -= Math.min(
+      itemAmount($item`asbestos ore`),
+      itemAmount($item`chrome ore`),
+      itemAmount($item`linoleum ore`)
+    );
+  }
   if (have($item`Deck of Every Card`) && get("_deckCardsDrawn") === 0) ore_needed--;
   const pulled = new Set<Item>(
     get("_roninStoragePulls")
