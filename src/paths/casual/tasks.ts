6e8f8548f33646ca8c89102304837377e0b84950
Kasekopf <jeffreydudek@gmail.com>
2025-02-23 22:47:21 -0800

    Actually max ML when summoning whelps

--- src/paths/casual/tasks.ts
+++ src/paths/casual/tasks.ts
@@ -117,6 +117,7 @@ export const CasualQuest: Quest = {
             $item`barrel lid`,
             $item`carnivorous potted plant`
           ),
+          modifier: "ML",
           modes: { retrocape: ["vampire", "kill"], umbrella: "broken" },
           skipDefaults: true,
         },
