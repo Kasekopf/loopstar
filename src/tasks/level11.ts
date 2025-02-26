d82dd6dcd70d96dba22956105791a5481f270234
Kasekopf <jeffreydudek@gmail.com>
2025-02-25 19:32:10 -0800

    Only buy the compass after using milestones

--- src/tasks/level11.ts
+++ src/tasks/level11.ts
@@ -147,7 +147,8 @@ const Desert: Task[] = [
   {
     name: "Compass",
     after: ["Misc/Unlock Beach", "Scrip"],
-    completed: () => have($item`UV-resistant compass`),
+    ready: () => !have($item`milestone`),
+    completed: () => have($item`UV-resistant compass`) || get("desertExploration") >= 100,
     do: () => buy($coinmaster`The Shore, Inc. Gift Shop`, 1, $item`UV-resistant compass`),
     limit: { tries: 1 },
     freeaction: true,
