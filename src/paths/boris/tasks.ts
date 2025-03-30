f1641618cc5ad245af6a4cae76df1c9f28f2bd29
Kasekopf <jeffreydudek@gmail.com>
2025-03-30 00:35:34 -0700

    Add basic food tasks

--- src/paths/boris/tasks.ts
+++ src/paths/boris/tasks.ts
@@ -1,14 +1,48 @@
 import { NamedDeltaTask, Quest } from "../../engine/task";
 import { toTempPref } from "../../args";
-import { cliExecute, min, myLevel, myTurncount, runChoice, visitUrl } from "kolmafia";
+import {
+  cliExecute,
+  drink,
+  eat,
+  fullnessLimit,
+  itemAmount,
+  min,
+  myAdventures,
+  myAscensions,
+  myFullness,
+  myInebriety,
+  myLevel,
+  myMeat,
+  myTurncount,
+  retrieveItem,
+  runChoice,
+  use,
+  useSkill,
+  visitUrl,
+} from "kolmafia";
 import { set } from "libram/dist/counter";
-import { $item, $items, $location, $monster, $skill, get, have, TrainSet, Witchess } from "libram";
+import {
+  $effect,
+  $item,
+  $items,
+  $location,
+  $monster,
+  $skill,
+  AugustScepter,
+  clamp,
+  directlyUse,
+  get,
+  have,
+  TrainSet,
+  Witchess,
+} from "libram";
 import { CombatStrategy } from "../../engine/combat";
 import { fillHp } from "../../engine/moods";
 import { Priorities } from "../../engine/priority";
 import { Station } from "libram/dist/resources/2022/TrainSet";
-import { haveOre } from "../../tasks/misc";
+import { haveOre, trainSetAvailable } from "../../tasks/misc";
 import { step } from "grimoire-kolmafia";
+import { atLevel } from "../../lib";
 
 const deletedTasks = [
   "Misc/Snojo",
@@ -60,6 +94,19 @@ export const borisDeltas: NamedDeltaTask[] = [
       },
       do: () => {
         TrainSet.setConfiguration(getDesiredTrainsetConfig());
+      },
+    },
+  },
+  {
+    name: "Keys/Deck",
+    replace: {
+      do: () => {
+        cliExecute("cheat tower");
+        if (get("_deckCardsDrawn") <= 10) cliExecute("cheat sheep");
+        if (get("_deckCardsDrawn") <= 10) {
+          if (trainSetAvailable()) cliExecute("cheat mickey");
+          else cliExecute("cheat mine");
+        }
       },
     },
   },
@@ -195,6 +242,187 @@ export const BorisQuest: Quest = {
         get("_boomBoxSongsLeft") === 0,
       do: () => cliExecute("boombox seasoning"),
       freeaction: true,
+      limit: { tries: 1 },
+    },
+  ],
+};
+
+export const BorisDietQuest: Quest = {
+  name: "BorisDiet",
+  tasks: [
+    {
+      name: "LOV Chocolate",
+      after: ["Boris/LOV Tunnel"],
+      ready: () => have($item`LOV Extraterrestrial Chocolate`),
+      completed: () => get("_loveChocolatesUsed") > 0,
+      do: () => use($item`LOV Extraterrestrial Chocolate`),
+      limit: { tries: 1 },
+      withnoadventures: true,
+    },
+    {
+      name: "Sausage",
+      after: ["Consume"],
+      completed: () => !have($item`Kramco Sausage-o-Matic™`) || get("_sausagesEaten") >= 23,
+      ready: () => have($item`magical sausage casing`) && myMeat() >= 10000,
+      do: (): void => {
+        const toEat = clamp(
+          itemAmount($item`magical sausage casing`),
+          0,
+          23 - get("_sausagesEaten")
+        );
+        eat(toEat, $item`magical sausage`);
+      },
+      limit: { tries: 23 },
+      freeaction: true,
+      withnoadventures: true,
+    },
+    // Drinking
+    {
+      name: "Open Pilsner",
+      after: [],
+      completed: () => !have($item`astral six-pack`),
+      do: () => use($item`astral six-pack`),
+      limit: { tries: 1 },
+      freeaction: true,
+    },
+    {
+      name: "Sweat Out",
+      after: [],
+      ready: () => myInebriety() > 0 && get("sweat") >= 25,
+      completed: () => !have($item`designer sweatpants`) || get("_sweatOutSomeBoozeUsed") >= 3,
+      do: () => useSkill($skill`Sweat Out Some Booze`),
+      limit: { tries: 3 },
+      freeaction: true,
+    },
+    {
+      name: "Pilsner",
+      after: ["Open Pilsner"],
+      ready: () => atLevel(11) && myInebriety() < 4,
+      completed: () => !have($item`astral pilsner`),
+      prepare: () => {
+        if (have($item`pocket wish`) && !have($effect`Ode to Booze`))
+          cliExecute("genie effect ode to booze");
+      },
+      do: () => drink($item`astral pilsner`),
+      limit: { tries: 6 },
+      freeaction: true,
+      withnoadventures: true,
+    },
+    {
+      name: "Martini",
+      after: ["Open Pilsner", "Pilsner"],
+      ready: () => myInebriety() < 4,
+      completed: () => !have($item`splendid martini`),
+      do: () => drink($item`splendid martini`),
+      limit: { tries: 1 },
+      freeaction: true,
+      withnoadventures: true,
+    },
+    // Eating
+    {
+      name: "Roller Coaster",
+      after: [],
+      ready: () => myFullness() >= 1,
+      completed: () => !AugustScepter.canCast(16),
+      do: () => useSkill($skill`Aug. 16th: Roller Coaster Day!`),
+      limit: { tries: 1 },
+      freeaction: true,
+    },
+    {
+      name: "Voraci Tea",
+      after: [],
+      ready: () => myFullness() >= 1,
+      completed: () => !have($item`cuppa Voraci tea`) && get("_pottedTeaTreeUsed"),
+      do: () => {
+        if (!have($item`cuppa Voraci tea`)) retrieveItem($item`cuppa Voraci tea`);
+        use($item`cuppa Voraci tea`);
+      },
+      limit: { tries: 1 },
+      freeaction: true,
+    },
+    {
+      name: "Horseradish",
+      after: [],
+      ready: () =>
+        (have($item`Special Seasoning`) || myAdventures() === 0) && have($skill`Gourmand`),
+      completed: () => !have($item`jumping horseradish`) || myFullness() >= fullnessLimit(),
+      do: () => eat($item`jumping horseradish`),
+      limit: { tries: 15 },
+      freeaction: true,
+    },
+    {
+      name: "Ice Rice",
+      after: ["Boris/Snojo"],
+      ready: () =>
+        (have($item`Special Seasoning`) || myAdventures() === 0) && have($skill`Gourmand`),
+      completed: () => !have($item`ice rice`) || myFullness() >= fullnessLimit(),
+      prepare: () => {
+        if (have($item`pocket wish`) && !have($effect`Got Milk`) && !get("_milkOfMagnesiumUsed"))
+          cliExecute("genie effect got milk");
+      },
+      do: () => eat($item`ice rice`),
+      limit: { tries: 1 },
+      freeaction: true,
+    },
+    {
+      name: "Timespun Ice Rice",
+      after: ["Boris/Snojo", "Ice Rice"],
+      completed: () =>
+        !have($item`Time-Spinner`) ||
+        get("_timeSpinnerMinutesUsed") >= 8 ||
+        myFullness() >= fullnessLimit(),
+      do: () => {
+        directlyUse($item`Time-Spinner`);
+        runChoice(2);
+        runChoice(1, `foodid=8697`);
+      },
+      limit: { tries: 3 },
+      freeaction: true,
+    },
+    {
+      name: "Cookbookbat Big",
+      after: [],
+      ready: () =>
+        itemAmount($item`Vegetable of Jarlsberg`) >= 2 &&
+        itemAmount($item`St. Sneaky Pete's Whey`) >= 2 &&
+        itemAmount($item`Yeast of Boris`) >= 2,
+      completed: () => get("deepDishOfLegendEaten") || myFullness() >= fullnessLimit(),
+      do: () => {
+        retrieveItem($item`Deep Dish of Legend`);
+        eat($item`Deep Dish of Legend`);
+      },
+      limit: { tries: 1 },
+      freeaction: true,
+    },
+    {
+      name: "Cookbookbat Small",
+      after: ["Cookbookbat Big"],
+      completed: () => itemAmount($item`Yeast of Boris`) < 2 || myFullness() >= fullnessLimit(),
+      do: () => {
+        retrieveItem($item`Boris's bread`);
+        eat($item`Boris's bread`);
+      },
+      limit: { tries: 1 },
+      freeaction: true,
+    },
+    {
+      name: "Borrowed Time",
+      after: [],
+      completed: () => !have($skill`Summon Clip Art`) || get("tomeSummons") >= 3,
+      do: () => {
+        if (!have($item`borrowed time`)) retrieveItem($item`borrowed time`);
+        use($item`borrowed time`);
+      },
+      limit: { tries: 1 },
+      freeaction: true,
+    },
+    {
+      name: "Such Great Heights",
+      after: ["Hidden City/Open City"],
+      ready: () => have($item`stone wool`),
+      completed: () => get("lastTempleAdventures") >= myAscensions(),
+      do: $location`The Hidden Temple`,
+      choices: { 582: 1, 579: 3 },
       limit: { tries: 1 },
     },
   ],
