6aaf92dcdaa8a78de6e315014b3d755fcbaf6846
Kasekopf <jeffreydudek@gmail.com>
2025-05-04 02:06:38 -0700

    Add list of swappers

---
+++ src/resources/swappers.ts
@@ -0,0 +1,22 @@
+import { $item, $skill, get, have } from "libram";
+import { CombatResource } from "./lib";
+import { inCasual } from "kolmafia";
+
+export const swapperSources: CombatResource[] = [
+  {
+    name: "Waffle",
+    do: $item`waffle`,
+    available: () => have($item`waffle`) && !inCasual(),
+  },
+  {
+    name: "Macrometeorite",
+    do: $skill`Macrometeorite`,
+    available: () => have($skill`Macrometeorite`) && get("_macrometeoriteUses") < 10,
+  },
+  {
+    name: "Power Glove",
+    do: $skill`CHEAT CODE: Replace Enemy`,
+    equip: $item`Powerful Glove`,
+    available: () => have($item`Powerful Glove`) && get("_powerfulGloveBatteryPowerUsed") <= 90,
+  }
+];
