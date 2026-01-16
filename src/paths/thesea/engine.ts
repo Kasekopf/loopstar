e0555bef4a65023e44bfe98424a68c2c0a189ef9
Ignose <ihpares@gmail.com>
2026-01-16 14:13:04 -0500

    seaLasso, use seaCombat

--- src/paths/thesea/engine.ts
+++ src/paths/thesea/engine.ts
@@ -1,9 +1,19 @@
 import { $effect, have } from "libram";
-import { Engine } from "../../engine/engine";
+import { ActiveTask, Engine } from "../../engine/engine";
 import { doFirstAvailableFishySource } from "./lib";
+import { Task } from "../../engine/task";
+import { EngineOptions } from "grimoire-kolmafia";
+import { CombatActions } from "../../engine/combat";
+import { SeaActionDefaults } from "./combat";
 
 
 export class SeaEngine extends Engine {
+
+  constructor(tasks: Task[], options: EngineOptions<CombatActions, ActiveTask> = {}) {
+    if (!options.combat_defaults) options.combat_defaults = new SeaActionDefaults();
+    super(tasks, options);
+  }
+
   override prepare(): void {
     if (!have($effect`Fishy`) && !have($effect`Driving Waterproofly`)) {
       doFirstAvailableFishySource();
