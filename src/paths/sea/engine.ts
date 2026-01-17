d480436b869bf1fc7a817e59d1391a7ef88e59f5
Ignose <ihpares@gmail.com>
2026-01-16 20:51:34 -0500

    First pass

--- src/paths/sea/engine.ts
+++ src/paths/sea/engine.ts
@@ -1,18 +1,32 @@
-import { CombatResources, Outfit } from "grimoire-kolmafia";
+import { CombatResources, EngineOptions, Outfit } from "grimoire-kolmafia";
 import { ActiveTask, Engine } from "../../engine/engine";
 import { CombatActions, CombatStrategy } from "../../engine/combat";
-import { $monsters, PropertiesManager, undelay } from "libram";
+import { $effect, $monsters, have, PropertiesManager, undelay } from "libram";
 import { Task } from "../../engine/task";
+import { doFirstAvailableWaterBreathSource } from "./util";
+import { SeaActionDefaults } from "./combat";
 
 export class TheSeaEngine extends Engine {
+  constructor(
+    tasks: Task[],
+    options: EngineOptions<CombatActions, ActiveTask> = {}
+  ) {
+    if (!options.combat_defaults) {
+      options.combat_defaults = new SeaActionDefaults();
+    }
+    super(tasks, options);
+  }
+
   override customize(
     task: ActiveTask,
     outfit: Outfit,
     combat: CombatStrategy,
     resources: CombatResources<CombatActions>
   ): void {
+    // Add your custom combat behavior
     combat.action("killHard", $monsters`time cop`);
 
+    // Let the base engine do its thing
     super.customize(task, outfit, combat, resources);
   }
 
@@ -20,13 +34,20 @@ export class TheSeaEngine extends Engine {
     const spec = undelay(task.outfit);
     const outfit = new Outfit();
 
-    if (spec !== undefined) outfit.equip(spec); // no error on failure
+    if (spec !== undefined) outfit.equip(spec); // ignore equip failures
     return outfit;
+  }
+
+  override prepare(): void {
+    if (!have($effect`Driving Waterproofly`)) {
+      doFirstAvailableWaterBreathSource();
+    }
   }
 
   override setChoices(task: ActiveTask, manager: PropertiesManager): void {
     super.setChoices(task, manager);
-    this.propertyManager.setChoices({
+
+    manager.setChoices({
       1528: 1,
     });
   }
--- src/paths/sea/util.ts
+++ src/paths/sea/util.ts
@@ -1,10 +1,41 @@
-import { buyUsingStorage, cliExecute, Item, abort, storageAmount } from "kolmafia";
+import { OutfitSpec } from "grimoire-kolmafia";
+import { buyUsingStorage, cliExecute, Item, abort, storageAmount, Skill, getWorkshed } from "kolmafia";
+import { $effect, $item, AsdonMartin, have } from "libram";
 
 export function pull(item: Item) {
-  if (storageAmount(item) === 0) {
-    if (buyUsingStorage(item, 1, 15000) === 0) {
-      abort(`Unable to buy desired pull item ${item.name}`);
+    if (storageAmount(item) === 0) {
+        if (buyUsingStorage(item, 1, 15000) === 0) {
+            abort(`Unable to buy desired pull item ${item.name}`);
+        }
     }
-  }
-  cliExecute(`pull ${item.name}`);
+    cliExecute(`pull ${item.name}`);
+}
+
+export type WaterBreathSource = {
+    name: Item | Skill;
+    available?: () => boolean;
+    outfit?: OutfitSpec | (() => OutfitSpec);
+    do?: () => void;
+};
+
+export function getWaterBreathSources(): WaterBreathSource[] {
+    return [
+        {
+            name: $item`Asdon Martin keyfob (on ring)`,
+            available: () => have($item`Asdon Martin keyfob (on ring)`) || getWorkshed() === $item`Asdon Martin keyfob (on ring)`,
+            do: () => {
+                AsdonMartin.drive($effect`Driving Waterproofly`)
+            }
+        }
+    ]
+}
+
+export function doFirstAvailableWaterBreathSource(): boolean {
+    for (const source of getWaterBreathSources()) {
+        if (source.available?.() ?? true) {
+            source.do?.();
+            return true;
+        }
+    }
+    return false;
 }
